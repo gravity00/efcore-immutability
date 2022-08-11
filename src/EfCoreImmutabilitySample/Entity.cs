cf495bd7975c57e8cdbd837045c8be543d86b778
João Simões <gravity00@users.noreply.github.com>
2022-08-11 09:54:09 +0100

    included docs for entities

--- src/EfCoreImmutabilitySample/Entity.cs
+++ src/EfCoreImmutabilitySample/Entity.cs
@@ -1,6 +1,12 @@
 ﻿namespace EfCoreImmutabilitySample;
 
+/// <summary>
+/// Base class for all database entities
+/// </summary>
 public abstract record Entity
 {
+    /// <summary>
+    /// Unique identifier
+    /// </summary>
     public long Id { get; init; }
 }
--- src/EfCoreImmutabilitySample/PersonEntity.cs
+++ src/EfCoreImmutabilitySample/PersonEntity.cs
@@ -1,11 +1,22 @@
 ﻿namespace EfCoreImmutabilitySample;
 
+/// <summary>
+/// Representation of a person
+/// </summary>
+/// <param name="Forename">Person first name</param>
+/// <param name="Surname">Person last name</param>
 public record PersonEntity(
     string Forename,
     string Surname
 ) : Entity
 {
+    /// <summary>
+    /// Person middle names, separated by spaces
+    /// </summary>
     public string? MiddleNames { get; init; }
 
+    /// <summary>
+    /// Person date of birth
+    /// </summary>
     public DateOnly? Birthdate { get; init; }
 }
